9f498d2408e764a774ddf251dbec23bdc2a0694c
Aqeel Husny <aqeel@moresandtechnologies.com>
2026-06-01 18:34:32 +0530

    Update text domain from 'aqeelhusny-min-order-gateway' to 'woo-min-order-payment'

--- aqeelhusny-min-order-gateway.php
+++ aqeelhusny-min-order-gateway.php
@@ -8,7 +8,7 @@
  * Author URI:        https://profiles.wordpress.org/aqeelhusny/
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       aqeelhusny-min-order-gateway
+ * Text Domain:       woo-min-order-payment
  * Domain Path:       /languages
  * Requires at least: 7.0
  * Requires PHP:      8.1
